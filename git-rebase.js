6684a082eece65cccbb88e921e592ab650d3906f
chenxianlong <794465731@qq.com>
2019-01-31 16:17:55 +0800

    test: merge hook

--- git-rebase.js
+++ git-rebase.js
@@ -1,1 +1,6 @@
-console.log('----has commit-msg----')
+console.log('----pre-rebase----')
+console.log()
+console.log(
+  '----use git-pull to trigger post-merge hook and sync code to neitui-webapp----'
+)
+process.exit(1)
